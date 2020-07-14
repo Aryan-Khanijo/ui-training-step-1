caa871eca7210351bf0c598afc49ee285b38a228
Aryan Khanijo <akhanijo12345@gmail.com>
2020-07-14 19:11:25 +0530

    inc dec function

--- static/script.js
+++ static/script.js
@@ -2,3 +2,22 @@ function like() {
     var element = document.getElementById("like");
     element.classList.toggle("clicked");
   } 
+
+function inc(id) {
+  var qty = parseInt(document.getElementById(id).innerText);
+  qty = qty + 1;
+  document.getElementById(id).innerText = qty;
+}
+
+function dec(id) {
+  var qty = parseInt(document.getElementById(id).innerText);
+  qty = qty - 1;
+  if (qty<0)
+  {
+    document.getElementById(id).innerText = 0;
+  }
+  else
+  {
+    document.getElementById(id).innerText = qty;
+  }
+}
